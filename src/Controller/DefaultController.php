7c5fb9cdeb069f9b6435f2dede29f30623dc404d
Jordi García Pons <jordigar00@gmail.com>
2023-01-17 23:44:06 +0100

    Maquetació del footer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,15 +10,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    # Per a poder fer les migracions i utilitzar les bases de dades
+    # cal executar en el terminal el comandament:
+    # 'sudo docker exec -it 2023-truiter-symfony_web-server_1 /bin/bash'
+    # si es treballa en l'equip local de casa el nom del servici es 2023-truiter-symfony-web-server-1
+
+    # Per a comprovar el nom dels contenidors de docker gastar comandament:
+    # 'sudo docker ps'
+
     #[Route('/home', name: 'index')]
     public function index(UsuarioRepository $usuarioRepository): Response
     {
         $message = "Benvingut a la tenda";
-        $usuarios = $usuarioRepository->findOneBy([], ['username' => 'DESC']);
+        $usuarios = $usuarioRepository->findOneBy([], ['username' => 'ASC']);
+
+        dump($usuarios);
 
         return $this->render('default.html.twig', [
             'message' => $message,
-            'usuario' => $usuarios,
+            #'usuario' => $usuarios,
         ]);
     }
 }
