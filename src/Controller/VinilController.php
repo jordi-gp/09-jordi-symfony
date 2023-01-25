907644b1308dda891035be130d6fd8a999ac5061
Jordi García Pons <jordigar00@gmail.com>
2023-01-25 09:18:31 +0100

    Maquetació Pàgina Vinil

--- src/Controller/VinilController.php
+++ src/Controller/VinilController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Vinilo;
+use App\Repository\ViniloRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VinilController extends AbstractController
 {
-    #[Route('/vinil/{name}', name: 'concrete_vinil')]
-    public function concrete_vinil(int $id): Response
+    #[Route('/vinil/id={id}', name: 'concrete_vinil')]
+    public function concrete_vinil(ViniloRepository $viniloRepository, Vinilo $vinilo): Response
     {
-        $text = "Vinil amb id $id";
+        $vinil = $viniloRepository->findOneBy(['id' => $vinilo]);
 
         return $this->render('vinil/_concreteVinil.html.twig', [
-            'message' => $text,
+            'vinilo' => $vinil,
         ]);
     }
 }
