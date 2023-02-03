46f9d2ff5572a2c3db555a805affa7e623495a14
Jordi García <jordigar00@gmail.com>
2023-02-03 08:53:16 +0100

    Sessió de Testing en classe

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,6 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true]
+    Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
